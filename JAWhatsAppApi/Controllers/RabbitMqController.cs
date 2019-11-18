1a7075b42bc57999e61de8a8068a1ddd45cad22f
Vikas B <vikas.b@justanswer.com>
2019-11-18 20:49:02 +0530

    RabbitMqController

---
+++ JAWhatsAppApi/Controllers/RabbitMqController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAWhatsAppApi.Common;
+using JAWhatsAppApi.Models;
+using JAWhatsAppApi.RabbitMq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JAWhatsAppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RabbitMqController : ControllerBase
+    {
+
+        private IOptions<RMQConfig> _configuration;
+        private Sender _sender;
+
+        public RabbitMqController(IOptions<RMQConfig> configuration)
+        {
+            _configuration = configuration;
+            _sender = new Sender();
+        }
+
+        public string Index()
+        {
+            return "RabitMQ is  running";
+        }
+
+        [HttpPost]
+        [Route("SendMessageToRabbitQueue")]
+         public ActionResult SendMessageToRabbitQueue(SendSmsInput sendSmsInput)
+        {
+            _sender.sendSms(_configuration);
+            return null;
+        }
+
+
+    }
+}
---
+++ JAWhatsAppApi/RabbitMq/Sender.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using JAWhatsAppApi.Models;
+using JAWhatsAppApi.RabbitMq;
+
+namespace JAWhatsAppApi.RabbitMq
+{
+    public class Sender
+    {
+        public void sendSms(RMQConfig configuration)
+        {
+            var factory = new ConnectionFactory() { HostName = configuration.HostName };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+
+
+                string message = "Testing RMQ ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
