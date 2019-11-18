e5e7b3c75bafb2e486d5a03d1c4d525cac808548
Akash Mane <akash.mane@justanswer.com>
2019-11-18 19:42:19 +0530

    Removed unnecassary code

--- JAWhatsAppApi/Controllers/SmsController.cs
+++ JAWhatsAppApi/Controllers/SmsController.cs
@@ -35,63 +35,7 @@ namespace CoreWebApp.Controllers
             return "Whatsapp api running";
         }
 
-        [HttpGet]
-        [Route("ReadMessageFromRMQ")]
-        public ActionResult ReadMessageFromRMQ()
-        {
-            /*  var factory = new ConnectionFactory() { HostName = "4dc06ccf.ngrok.io" , UserName = "guest", Password = "guest"};
-              var message = "";
-              using (var connection = factory.CreateConnection())
-              using (var channel = connection.CreateModel())
-              {
-                  channel.ExchangeDeclare(exchange: "testExchange", type: ExchangeType.Direct);
 
-                  var queueName = channel.QueueDeclare().QueueName;
-                  channel.QueueBind(queue: queueName,
-                      exchange: "testExchange",
-                      routingKey: "");
-
-                  Console.WriteLine(" [*] Waiting for logs.");
-
-                  var consumer = new EventingBasicConsumer(channel);
-                  consumer.Received += (model, ea) =>
-                  {
-                      var body = ea.Body;
-                       message = Encoding.UTF8.GetString(body);
-
-                      Console.WriteLine(" [x] {0}", message);
-                  };
-                  channel.BasicConsume(queue: queueName,
-                      autoAck: true,
-                      consumer: consumer);
-
-                  Console.WriteLine(" Press [enter] to exit.");
-                  Console.ReadLine();
-              }*/
-
-            var factory = new ConnectionFactory()
-            { HostName = "4dc06ccf.ngrok.io" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: "testFromCode",
-                    durable: false,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
-
-                string message = "Hello World!";
-                var body = Encoding.UTF8.GetBytes(message);
-
-                channel.BasicPublish(exchange: "",
-                    routingKey: "hello",
-                    basicProperties: null,
-                    body: body);
-                Console.WriteLine(" [x] Sent {0}", message);
-
-                return Content(message);
-            }
-        }
 
         [HttpPost]
         [Route("SendSms")]
@@ -102,13 +46,6 @@ namespace CoreWebApp.Controllers
             var fromNumber = _configuration.Value.FromNumber;
 
             TwilioClient.Init(accountSid, authToken);
-
-            /*var messageOptions = new CreateMessageOptions(
-                new PhoneNumber(WhatsAppConstants.WHATSAPPPREFIX + sendSmsInput.ToNumber));
-            messageOptions.From = new PhoneNumber(WhatsAppConstants.WHATSAPPPREFIX + fromNumber);
-            messageOptions.Body = sendSmsInput.MessageBody;
-
-            var message = MessageResource.Create(messageOptions);*/
 
             var message = MessageResource.Create(
                 body: sendSmsInput.MessageBody,
